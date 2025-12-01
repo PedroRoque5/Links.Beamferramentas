fc8865d90eacdd231e5dd319f3e12a2ca0713d09
PedroRoque5 <pedroeduardoroque01@gmail.com>
2025-12-01 09:37:44 -0300

    Primeiro Commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Links Beam</title>
+    <link rel="stylesheet" href="style.css" />
+    <script src="script.js"></script>
+  </head>
+  <body>
+    <img src="https://uploads.onecompiler.io/445wa6jkt/445w9utzh/image-base64-normal-hsVagx7Z-27112024113228491.png" class="image" alt="" />
+    <p class="info_loja">Rua Celestino Lopes Bahia, 920, Vila São Luiz - Ourinhos - SP</p>
+   
+    <div class="container">   
+      <div class="links">
+
+      <a href="https://www.facebook.com/profile.php?id=100091468114901&locale=pt_BR" class="btn">
+        <img src="https://uploads.onecompiler.io/445wa6jkt/445w9utzh/pngtree-facebook-logo-facebook-icon-png-image_3654755%20(1).png" alt="">
+        <span>Facebook</span>
+      </a>
+      
+      <a href="https://www.facebook.com/profile.php?id=61569694191630&locale=pt_BR" class="btn">
+        <img src="https://uploads.onecompiler.io/445wa6jkt/445w9utzh/pngtree-facebook-logo-facebook-icon-png-image_3654755%20(1).png" alt="">
+        <span>Página do Facebook</span>
+      </a>
+      
+      <a href="https://www.instagram.com/beam_parafusoseferramentas/" class="btn">
+        <img src="https://uploads.onecompiler.io/445wa6jkt/445w9utzh/pngtree-instagram-icon-collection-png-image_5320951.png" alt="">
+        <span>Instagram</span>
+      </a>
+      
+      <a href="https://www.beamferramentas.com.br/" class="btn">
+        <img src="https://uploads.onecompiler.io/445wa6jkt/445w9utzh/9e5a4a16e78a187fc3e47fc6e2c5f03a-traco-do-icone-do-site-da-internet.webp" alt="">
+        <span>Site</span>
+      </a>
+      
+      <a href="https://l.instagram.com/?u=https%3A%2F%2Fwa.me%2F5514997062058%3Futm_source%3Dig%26utm_medium%3Dsocial%26utm_content%3Dlink_in_bio%26fbclid%3DPAZXh0bgNhZW0CMTEAc3J0YwZhcHBfaWQMMjU2MjgxMDQwNTU4AAGnwFggFbh6WFKYz18nK4eA65frIkAH02RsLamjW1qTSBfQmON_z8kdH0NAF5U_aem_jTh_6lsuWdB5T5E3LD1YJA&e=AT3FQSuJKjfa_aOdQ-veatMVg8vxIBXXB4j8h9zNdreEK4LKCbSxjs5Lzx6Fusxxh6OnLrAegNj05i6H3rFR1njqnuLJL7NCAy4F6LSMWdcU3RbT6tZ959bUPg" class="btn">
+        <img src="https://uploads.onecompiler.io/445wa6jkt/445w9utzh/sm_5b321c99945a2.png" alt="">
+        <span>WhatsApp</span>
+      </a>
+
+      </div>
+    </div>
+
+  </body>
+</html>
